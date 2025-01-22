1dbcd015d6d116dca5817736d172f84cebee59c3
ihwanariga07 <ihwanariga.ia@gmail.com>
2025-01-22 10:15:42 +0700

    menambah index.php dan navbar.php

---
+++ navbar.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg bg-info-subtle">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="/PROJECT/logo/internet.png" alt="Brand Logo" type="image" width="50" height="50">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link menu active" aria-current="page" href="">Beranda</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menu active" aria-current="page" href="">Data Mahasiswa</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menu active" aria-current="page" href="">Data Jurusan</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link menu active" aria-current="page" href="">Data Dosen</a>
+        </li>
+      </ul>
+      <form class="d-flex mx-auto me-50" role="search" style="width: 50%;">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+
+      <a 
+        class="btn btn-danger nav-link text-black" 
+        href="/uasihwan/frontpage.php" 
+        onclick="return confirmLogout(event);"
+      >Logout</a>
+    </div>
+  </div>
+</nav>

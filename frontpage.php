e73ade3683a73333c09d387a8438b20cbdb6c4b3
ihwanariga07 <ihwanariga.ia@gmail.com>
2025-01-22 10:07:59 +0700

    menambah kan bootstrap

---
+++ frontpage.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UAS IHWAN ARIGA</title>
+    <style>
+
+</style>
+</head>
+<body>
+    <!-- Background animasi -->
+    <div id="animated-background"></div>
+
+    <!-- Kontainer utama -->
+    <div class="container">
+        <!-- Gambar Profil -->
+        <img src="/PROJECT/logo/internet.png" alt="default">
+        
+        <!-- Judul -->
+        <h1>Selamat Datang</h1>
+        
+        <!-- Deskripsi -->
+        <p>Di Hotel Renggali Gayo HighLind.</p>
+        
+  
+        <!-- Tombol Login dan Register -->
+        <div class="button-container">
+            <form action="login.php" method="post" style="display: inline;">
+                <button type="submit">Login</button>
+            </form>
+            <form action="akun/formakun.php" method="get" style="display: inline;">
+                <button type="submit" class="register">Register</button>
+            </form>
+        </div>
+    </div>
+
+    <script src="" crossorigin=""></script>
+</body>
+</html>
